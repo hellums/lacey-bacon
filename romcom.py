8516f80f0880f3a7fbc978ba7d920beed04f3494
hellums <83464025+hellums@users.noreply.github.com>
2022-01-21 12:01:33 -0500

    minor changes and cleanup to main()

--- romcom.py
+++ romcom.py
@@ -1,10 +1,82 @@
 # Import os module for system calls to cls and clear (screen)
-import os
+import os # for calls to clear screen
+import csv # to import TSV files for moive and actor lists
 
-# Function to print a main menu to loop through
+# Create classes, movie data courtesy of imdb.com (interface download)
+class Movie:  # Curated list of romantic comedies, mysteries, and dramas, primarily Hallmark originals
+  def __init__(self, movieId, movieTitle="", movieRating=None, movieGenres="", movieRuntime=None, movieType="", movieYear=None, movieDirector="", movieActors=""):      
+    self.Id = movieId
+    self.Title = movieTitle
+    self.Rating = movieRating
+    self.Genres = movieGenres 
+    self.Runtime = movieRuntime
+    self.Type = movieType       
+    self.Year = movieYear 
+    self.Director = movieDirector
+    self.Actors = movieActors     
+
+class Actor:  # Curated list, details of "Hallmark" leading actor and actress
+  def __init__(self, actorId, actorName="", actorBorn=None, actorDied=None):      
+    self.Id = actorId
+    self.Name = actorName
+    self.Born = actorBorn
+    self.Died = actorDied
+
+class Role:  # Curated list. all leading actors and actresses starring in Hallmark movie list
+  def __init__(self, movieId, actorId, actorRole):      
+    self.movieId = movieId
+    self.actorId = actorId
+    self.actorRole = actorRole
+
+class Rating:  # Curated list, rating details on "Hallmark" movies in list
+  def __init__(self, movieId, movieRating, movieVotes):      
+    self.movieId = movieId
+    self.movieRating = movieRating
+    self.movieVotes = movieVotes
+  
+# Initialize lists from associated files of curated "Hallmark" data
+movie_list = []  # title-basics-imdb.tsv
+rating_list = []  # title-ratings-imdb.tsv
+actor_list = []  # name-basics-imdb.tsv
+role_list = []  # title-actors-imdb.tsv
+
+# Load records from each file as class instances to each list
+with open('data/title-basics-imdb.tsv', 'r', encoding='utf8') as f:
+  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+  for row in data:
+    new_movie = Movie(row[0], row[1], row[2], row[3])
+    movie_list.append(new_movie) #add the data from the text file to the list
+
+rating_list = []
+with open('data/title-ratings-imdb.tsv', 'r', encoding='utf8') as f:
+  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+  for row in data:
+    new_rating = Rating(row[0], row[1], row[2])
+    rating_list.append(new_rating) #add the data from the text file to the list
+
+actor_list = []
+with open('data/name-basics-imdb.tsv', 'r', encoding='utf8') as f:
+  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+  for row in data:
+    new_actor = Actor(row[0], row[1], row[2], row[3])
+    actor_list.append(new_actor) #add the data from the text file to the list
+
+role_list = []
+with open('data/title-actors-imdb.tsv', 'r', encoding='utf8') as f:
+  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+  for row in data:
+    new_role = Rating(row[0], row[1], row[2])
+    role_list.append(new_role) #add the data from the text file to the list
+
+# Define function to print a main menu to loop through
 def print_menu():
 
-    # Dictionary for user menu item selection
+    # Validaate records loaded from file and addressable
+    #print (actor_list[0].Id)
+    #print (actor_list[1].Id)
+    #print (type(actor_list))
+
+    # Create dictionary for user menu and item selections
     menu_options = {
         1: 'Option 1',
         2: 'Option 2',
@@ -16,19 +88,16 @@ def print_menu():
     for key in menu_options.keys():
         print (key, '--', menu_options[key] )
 
-# Function #1, launched when chosed from main menu by user
+# Define functions launched when chosen from main menu by user
 def option1():
      print('\'Option 1\' selected.')
 
-# Function #2, launched when chosed from main menu by user
 def option2():
      print('\'Option 2\' selected')
 
-# Function #3, launched when chosed from main menu by user
 def option3():
      print('\'Option 3\' selected.')
 
-# Function to clear the screen using os.system
 def clrscr():
     # Check if Operating System is Mac and Linux or Windows
     if os.name == 'posix':
@@ -37,6 +106,7 @@ def clrscr():
         # Else Operating System is Windows (os.name = nt)
       _ = os.system('cls')
 
+# Define main function to print menu and get user choice
 def main():
     # Clear the screen
     clrscr()
