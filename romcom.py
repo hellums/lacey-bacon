1a6ef7c9f3a7eb136c0b70664b18f19cc31ee141
hellums <83464025+hellums@users.noreply.github.com>
2022-01-21 17:50:12 -0500

    rearranged file for readability, corrected typo in main py file

--- romcom.py
+++ romcom.py
@@ -1,9 +1,11 @@
 # Import os module for system calls to cls and clear (screen)
-import os # for calls to clear screen
-import csv # to import TSV files for moive and actor lists
+import os  # for system calls to clear screen
+import csv  # to import TSV files for movie and actor lists
+import my_classes  # for access to imdb movie, actor, class, rating objects
 
 # Create classes, movie data courtesy of imdb.com (interface download)
 class Movie:  # Curated list of romantic comedies, mysteries, and dramas, primarily Hallmark originals
+  
   def __init__(self, movieId, movieTitle="", movieRating=None, movieGenres="", movieRuntime=None, movieType="", movieYear=None, movieDirector="", movieActors=""):      
     self.Id = movieId
     self.Title = movieTitle
@@ -16,6 +18,7 @@ class Movie:  # Curated list of romantic comedies, mysteries, and dramas, primar
     self.Actors = movieActors     
 
 class Actor:  # Curated list, details of "Hallmark" leading actor and actress
+  
   def __init__(self, actorId, actorName="", actorBorn=None, actorDied=None):      
     self.Id = actorId
     self.Name = actorName
@@ -23,58 +26,56 @@ class Actor:  # Curated list, details of "Hallmark" leading actor and actress
     self.Died = actorDied
 
 class Role:  # Curated list. all leading actors and actresses starring in Hallmark movie list
+  
   def __init__(self, movieId, actorId, actorRole):      
     self.movieId = movieId
     self.actorId = actorId
     self.actorRole = actorRole
 
 class Rating:  # Curated list, rating details on "Hallmark" movies in list
+  
   def __init__(self, movieId, movieRating, movieVotes):      
     self.movieId = movieId
     self.movieRating = movieRating
     self.movieVotes = movieVotes
-  
-# Initialize lists from associated files of curated "Hallmark" data
-movie_list = []  # title-basics-imdb.tsv
-rating_list = []  # title-ratings-imdb.tsv
-actor_list = []  # name-basics-imdb.tsv
-role_list = []  # title-actors-imdb.tsv
-
+    
 # Load records from each file as class instances to each list
-with open('data/title-basics-imdb.tsv', 'r', encoding='utf8') as f:
-  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
-  for row in data:
-    new_movie = Movie(row[0], row[1], row[2], row[3])
-    movie_list.append(new_movie) #add the data from the text file to the list
+def load_data():  
 
-rating_list = []
-with open('data/title-ratings-imdb.tsv', 'r', encoding='utf8') as f:
-  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
-  for row in data:
-    new_rating = Rating(row[0], row[1], row[2])
-    rating_list.append(new_rating) #add the data from the text file to the list
+  global movie_list # title-basics-imdb.tsv
+  movie_list = []  
+  with open('data/title-basics-imdb.tsv', 'r', encoding='utf8') as f:
+    data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+    for row in data:
+      new_movie = Movie(row[0], row[1], row[2], row[3])
+      movie_list.append(new_movie) #add the data from the text file to the list
 
-actor_list = []
-with open('data/name-basics-imdb.tsv', 'r', encoding='utf8') as f:
-  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
-  for row in data:
-    new_actor = Actor(row[0], row[1], row[2], row[3])
-    actor_list.append(new_actor) #add the data from the text file to the list
+  global rating_list  # title-ratings-imdb.tsv 
+  rating_list = []  
+  with open('data/title-ratings-imdb.tsv', 'r', encoding='utf8') as f:
+    data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+    for row in data:
+      new_rating = Rating(row[0], row[1], row[2])
+      rating_list.append(new_rating) #add the data from the text file to the list
 
-role_list = []
-with open('data/title-actors-imdb.tsv', 'r', encoding='utf8') as f:
-  data = csv.reader(f, delimiter='\t') #read tsv text file with csv
-  for row in data:
-    new_role = Rating(row[0], row[1], row[2])
-    role_list.append(new_role) #add the data from the text file to the list
+  global actor_list  # name-basics-imdb.tsv
+  actor_list = []
+  with open('data/name-basics-imdb.tsv', 'r', encoding='utf8') as f:
+    data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+    for row in data:
+      new_actor = Actor(row[0], row[1], row[2], row[3])
+      actor_list.append(new_actor) #add the data from the text file to the list
+
+  global role_list  # title-actors-imdb.tsv
+  role_list = []
+  with open('data/title-actors-imdb.tsv', 'r', encoding='utf8') as f:
+    data = csv.reader(f, delimiter='\t') #read tsv text file with csv
+    for row in data:
+      new_role = Rating(row[0], row[1], row[2])
+      role_list.append(new_role) #add the data from the text file to the list
 
 # Define function to print a main menu to loop through
 def print_menu():
-
-    # Validaate records loaded from file and addressable
-    #print (actor_list[0].Id)
-    #print (actor_list[1].Id)
-    #print (type(actor_list))
 
     # Create dictionary for user menu and item selections
     menu_options = {
@@ -98,6 +99,26 @@ def option2():
 def option3():
      print('\'Option 3\' selected.')
 
+def option9():  # for debug only, to be removed later
+    # Validate records loaded from file and addressable
+        print ( actor_list[0].Id, actor_list[1].Id, type(actor_list), "\n" )
+        print ( "# actors: ", len(actor_list))
+        print ( "# movies: ", len(movie_list))
+        print ( "# ratings: ", len(rating_list))
+        print ( "# roles: ", len(role_list))
+        #print ( role_list.__sizeof__() )
+        #print ( dir(Actor) )
+        #next(item for item in movie_list if item["Id"] == "tt10921042", None)
+        total = 0.0
+        count = 0
+        average = 0.0
+        for line in rating_list:
+            total = total + float(line.movieRating)
+            count = count + 1
+        average = total/count
+        print("average rating:", format(average, '.1f') )
+            
+
 def clrscr():
     # Check if Operating System is Mac and Linux or Windows
     if os.name == 'posix':
@@ -108,8 +129,12 @@ def clrscr():
 
 # Define main function to print menu and get user choice
 def main():
+
     # Clear the screen
     clrscr()
+
+    # Load data from files into list of class objects
+    load_data()
 
     # Loop through main menu until user opts to exit
     while(True):
@@ -134,6 +159,9 @@ def main():
         elif option == 3:
             clrscr()
             option3()
+        elif option == 9:  # for debug only, to be removed later
+            clrscr()
+            option9()
         elif option == 4:
             clrscr()
             print('\'Option 4\' selected, our work is done here.')
