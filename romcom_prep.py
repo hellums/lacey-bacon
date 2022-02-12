97bcb07cec2a3844d38461075c29490a70a9fe19
Duane <duane@Duanes-MacBook-Air.local>
2022-02-11 21:09:55 -0500

    moved file download and uncompress functions to separate file from main module

---
+++ romcom_prep.py
@@ -0,0 +1,48 @@
+import requests #needs install
+import gzip
+
+def main():
+    print('\nThis process could take a few minutes, depending on Internet speed...')
+    remote_url ='https://raw.githubusercontent.com/hellums/lacey-bacon/root/watchlist.txt'  
+    local_file = 'watchlist.txt'  # export of imdb watchlist
+    download_file(remote_url, local_file)
+    remote_url ='https://datasets.imdbws.com/title.ratings.tsv.gz'
+    local_file = 'movie_ratings.tsv.gz'  # ratings and number votes, for some movies (not all)
+    download_file(remote_url, local_file)
+    uncompress_file(local_file, 'movie_ratings.tsv')
+    remote_url ='https://datasets.imdbws.com/title.basics.tsv.gz'  
+    local_file = 'movie_info.tsv.gz'  # detail and metadata about all imdb movies (pare down!)
+    download_file(remote_url, local_file)
+    uncompress_file(local_file, 'movie_info.tsv')
+    remote_url ='https://datasets.imdbws.com/name.basics.tsv.gz'
+    local_file = 'cast_crew_info.tsv.gz'  # personal details of cast and crew
+    download_file(remote_url, local_file)
+    uncompress_file(local_file, 'cast_crew_info.tsv')
+    remote_url ='https://datasets.imdbws.com/title.principals.tsv.gz'
+    local_file = 'movie_cast_crew.tsv.gz'  # list of major cast and crew for all movies (pare down!)
+    download_file(remote_url, local_file)
+    uncompress_file(local_file, 'movie_cast_crew.tsv')
+    remote_url ='https://datasets.imdbws.com/title.crew.tsv.gz'
+    local_file = 'movie_crew.tsv.gz'  # list of director and writers for all movies
+    download_file(remote_url, local_file)
+    uncompress_file(local_file, 'movie_crew.tsv')
+    return None  # results of download_uncompress_imdb_files
+
+def download_file(remote, local):
+    print('\nDownloading', local)
+    data = requests.get(remote)
+    with open(local, 'wb') as file:
+        file.write(data.content)
+    return None
+
+def uncompress_file(compressed, uncompressed):
+    print('\nUncompressing', uncompressed)
+    with gzip.open(compressed, 'rb') as f:
+        data = f.read()
+    with open(uncompressed, 'wb') as f:
+        f.write(data)
+    return None
+
+# Allow file to be used as function or program
+if __name__=='__main__':
+    main()
