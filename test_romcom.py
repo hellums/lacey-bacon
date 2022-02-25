1a430a8c0279d05d8994022e12fd77380211f73a
Duane Hellums <hellums@yahoo.com>
2022-02-25 15:39:50 -0500

    updated unittest file to reflect file/module name changes

--- test_romcom.py
+++ test_romcom.py
@@ -1,8 +1,8 @@
-# test_romcom.py 2/15/22 11:14 PM
+# test_romcom.py 2/25/22 3:38 PM
 import unittest 
 import romcom
-import romcom_sql  
-import romcom_prep
+import romcomSQL  
+import romcomPrep
 
 class TestSqlRecords(unittest.TestCase):  
 
@@ -11,7 +11,7 @@ class TestSqlRecords(unittest.TestCase):
     def test_table1_loaded(self):
         print('Checking movie table...')
         table_name = 'movie_info'
-        results = romcom_sql.count_records(table_name)
+        results = romcomSQL.count_records(table_name)
         for var in results:
             actual = var[0]
         expected = 1000
@@ -20,7 +20,7 @@ class TestSqlRecords(unittest.TestCase):
     def test_table2_loaded(self):
         print('Checking movie_cast table...')
         table_name = 'movie_cast_crew'
-        results = romcom_sql.count_records(table_name)
+        results = romcomSQL.count_records(table_name)
         for var in results:
             actual = var[0]
         expected = 4500  # should be 4702 or so
@@ -29,7 +29,7 @@ class TestSqlRecords(unittest.TestCase):
     def test_table3_loaded(self):
         print('Checking cast_crew table...')
         table_name = 'cast_crew_info'
-        results = romcom_sql.count_records(table_name)
+        results = romcomSQL.count_records(table_name)
         for var in results:
             actual = var[0]
         expected = 2300  # should be 2463 or so
@@ -38,14 +38,14 @@ class TestSqlRecords(unittest.TestCase):
     def test_table4_loaded(self):
         print('Checking leader_board table...')
         table_name = 'leader_board'
-        results = romcom_sql.count_records(table_name)
+        results = romcomSQL.count_records(table_name)
         for var in results:
             actual = var[0]
         expected = 2300  # should be 2463 or so
         self.assertLess(expected, actual, "leader_board < 2300 records")
     
     def test_watch_list(self):
-        results = list(romcom_prep.load_watchlist())
+        results = list(romcomPrep.load_watchlist())
         actual = len(results)  # should be 1141
         expected = 1000 
         self.assertLess(expected, actual, "Less than 1000 movies in list?")
