775f4f431ed5d7e1372d4dfd2cb1d8950e7fdac4
Duane Hellums <hellums@yahoo.com>
2022-02-27 15:04:24 -0500

    simplified append as list vs enumarated

--- romcomWeb.py
+++ romcomWeb.py
@@ -30,8 +30,8 @@ def actor_data():
             actor_nm = nm_lookup(actor_name)  # if we get results, we'll return them
             actor_movies = films_lookup(actor_nm)  # pull a list of this actor's movie title codes
             actor_titles = []
-            for k, v in enumerate(actor_movies):
-                actor_titles.append(tt_title[v])  # lookup the code to get titles
+            for each in actor_movies:
+                actor_titles.append(tt_title[each])  # lookup the code to get titles
             total_titles = len(actor_titles)
             shortest_path=lacey_sp[actor_name]
             separation=int(len(shortest_path)/2)
