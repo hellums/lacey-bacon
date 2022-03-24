808a43decd9e7a27d482fcdaf2e1ec6efe45256f
hellums <83464025+hellums@users.noreply.github.com>
2022-03-24 12:49:10 -0400

    updated batch file

--- run.bat
+++ run.bat
@@ -1,22 +1,21 @@
  @echo off
 echo.   
-echo First step is to set up environment and install prerequisites
+echo First step is to set up environment and install prerequisites. No interaction needed at this point.
 pause
 python -m venv env
 .\env\Scripts\pip install -r requirements.txt
 echo.
-echo Next step runs a few tests of SQL database and tables
+echo Next step runs a few tests of SQL database and tables. No interaction needed at this point.
 pause
 .\env\Scripts\python romcomSQL.py
 echo. 
-echo Next step runs test of data structures and functions
+echo Next step runs test of data structures and functions. No interaction needed at this point.
 pause
 .\env\Scripts\python -m unittest test_romcom
 echo.
-echo Next step runs the command line program main loop. When you exit, you will return here.
+echo Next step runs the command line program main loop. This is where you interact with the program. When you select quit, you will return here.
 pause
 .\env\Scripts\python romcom.py
-echo.
 echo Next step runs the Flask engine and launches the web interface in the default browser. Just close both windows after testing, and return here.
 pause
 start .\env\Scripts\python romcomWeb.py
